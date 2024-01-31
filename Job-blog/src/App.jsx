a716feef172c45bfa4d6860a01c01b4eead58e01
Christine-design-web <wakuthiichristine5@gmail.com>
2024-01-31 21:52:06 +0300

    set up is done

--- Job-blog/src/App.jsx
+++ Job-blog/src/App.jsx
@@ -1,9 +1,11 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
+
 function App() {
 
   return (
     <>
-    <h4 className='text-5x1 font-bold text-indigo-700'>Home</h4>
+   <Outlet/>
     </>
   )
 }
