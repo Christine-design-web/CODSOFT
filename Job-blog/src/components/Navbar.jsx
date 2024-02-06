567b056a89bf6595a4761c79b6c7c02c445211fe
Christine-design-web <wakuthiichristine5@gmail.com>
2024-02-06 05:20:32 +0300

    nav item for large devices

--- Job-blog/src/components/Navbar.jsx
+++ Job-blog/src/components/Navbar.jsx
@@ -5,14 +5,29 @@ const Navbar = () => {
   const handleMenuToggler = () => {
     setIsMenuOpen(isMenuOpen)
  
-  }
+  };
+  const navItems = [
+    {path: "/", title: "Start a search"},
+    {path: "/my-job", title: "My-jobs"},
+    {path: "/salary", title: "Salary Estimated"},
+    {path: "/post-job", title: "Post A Job"},
+  ]
   return (
     <header>
       <nav>
-        <a href="/" className="flex items-center gap-4 text-4xl">
+        <a href="/" className="flex items-center gap-4 text-4xl text-black">
           <span>Job Portal</span>
         
         </a>
+        {/* nav items for large devices */}
+        <ul>
+          {
+            navItems.map(({path, title}) => (
+              <li key={path}></li>
+            ) )
+          }
+        </ul>
+
       </nav>
     </header>
   )
