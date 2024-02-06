6894bdf6a68d9a48df3f3437f331f166cd8114cc
Christine-design-web <wakuthiichristine5@gmail.com>
2024-02-06 03:35:56 +0300

    tailwind css is failing

--- Job-blog/src/Pages/Home.jsx
+++ Job-blog/src/Pages/Home.jsx
@@ -1,7 +1,7 @@
-import React from "react"
+// import React from "react"
 const Home = () => {
   return (
-    <div>Home Page</div>
+    <div className="text-blue">Home Page</div>
   )
 }
 
--- Job-blog/tailwind.config.js
+++ Job-blog/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#141414",
+        "blue": "#35752E2"
+      }
+    },
   },
   plugins: [],
 }
