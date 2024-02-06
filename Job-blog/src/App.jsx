56628e55b25fb403c116cbf8d87b860af29040ac
githukelvin <kelvingithu019@gmail.com>
2024-02-06 08:01:31 +0300

    updated  your config and ailwind files

--- Job-blog/src/App.jsx
+++ Job-blog/src/App.jsx
@@ -2,10 +2,21 @@ import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 
+
+// export default function App() {
+//   return (
+//     <h1 className="text-3xl font-bold underline">
+//       Hello world!
+//     </h1>
+//   )
+// }
 function App() {
 
   return (
     <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
    <Navbar/>
    <Outlet/>
  
