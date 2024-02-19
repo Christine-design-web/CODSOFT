fe3c7bac4b52d69c746d627fe8c42c184782bb42
Christine-design-web <wakuthiichristine5@gmail.com>
2024-02-19 23:48:54 +0300

    fisrt banner

--- Job-blog/src/Pages/Home.jsx
+++ Job-blog/src/Pages/Home.jsx
@@ -1,8 +1,8 @@
 // import React from "react"
 const Home = () => {
   return (
-    <div className="max-w-screen-2xl container mx-auto xl:px-24 md:py20 py-14">
-      banner
+    <div className="max-w-screen-2xl container mx-auto xl:px-24 md:py-20 py-14">
+    <h1 className="text-5xl font-bold text-primary mb-3">Find your<span className="text-blue"> new Job</span> today</h1>
 
     </div>
 
