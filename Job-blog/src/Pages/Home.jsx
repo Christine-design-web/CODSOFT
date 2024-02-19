0c97e10837454f9e8206b280e985a76ef93c16f2
Christine-design-web <wakuthiichristine5@gmail.com>
2024-02-19 23:40:54 +0300

    banner text

--- Job-blog/src/Pages/Home.jsx
+++ Job-blog/src/Pages/Home.jsx
@@ -1,7 +1,10 @@
 // import React from "react"
 const Home = () => {
   return (
-    <div className="text-blue">Home Page</div>
+    <div className="max-w-screen-2xl container mx-auto xl:px-24 md:py20 py-14">
+      banner
+
+    </div>
 
   )
 }
