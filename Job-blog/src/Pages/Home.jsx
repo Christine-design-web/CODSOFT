c91fee185f59947143561cff318379c22c7d8ea2
Christine-design-web <wakuthiichristine5@gmail.com>
2024-02-19 23:57:26 +0300

    hello

--- Job-blog/src/Pages/Home.jsx
+++ Job-blog/src/Pages/Home.jsx
@@ -3,6 +3,7 @@ const Home = () => {
   return (
     <div className="max-w-screen-2xl container mx-auto xl:px-24 md:py-20 py-14">
     <h1 className="text-4xl font-bold text-primary mb-2">Find your<span className="text-blue"> new Job</span> today</h1>
+    <p> low key pushing</p>
 
     </div>
 
