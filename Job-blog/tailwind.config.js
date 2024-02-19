2f91b2d49b4e7094abf57ad4c20639dcb16c509f
Christine-design-web <wakuthiichristine5@gmail.com>
2024-02-19 12:14:48 +0300

    failed to load the colors

--- Job-blog/tailwind.config.js
+++ Job-blog/tailwind.config.js
@@ -7,9 +7,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        "bg-primary": "#141414",
+        "primary": "#141414",
         "blue": "#35752E",
         "purple": "#8A3FFC", // Example of adding a new color
+        "white": "#fff",
        
       }
     },
