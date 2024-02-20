7d9aaa082ecf5455ee049260ec974da3b0a4a375
Christine-design-web <wakuthiichristine5@gmail.com>
2024-02-20 09:36:33 +0300

    added the banner text

--- Job-blog/src/Pages/Home.jsx
+++ Job-blog/src/Pages/Home.jsx
@@ -1,12 +1,10 @@
 // import React from "react"
+import Banner from "../components/Banner"
 const Home = () => {
   return (
-    <div className="max-w-screen-2xl container mx-auto xl:px-24 md:py-20 py-14">
-    <h1 className="text-4xl font-bold text-primary mb-2">Find your<span className="text-blue"> new Job</span> today</h1>
-    <p></p>
-
-
-    </div>
+   <div>
+    <Banner/>
+   </div>
 
   )
 }
---
+++ Job-blog/src/components/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    
+       <div className="max-w-screen-2xl container mx-auto xl:px-24 md:py-20 py-14">
+    <h1 className="text-4xl font-bold text-primary mb-2">Find your<span className="text-blue"> new Job</span> today</h1>
+    <p className="text-lg text-black/70 mb-8">Thousands of jobs in the computer, engineering and technology sectors are waiting for you</p>
+
+
+    </div>
+    
+  )
+}
+
+export default Banner
