cafaed0d085d4c3c34f87204b7d542b7eedd1248
Christine-design-web <wakuthiichristine5@gmail.com>
2024-02-21 17:46:32 +0300

    json files added

--- Job-blog/src/Pages/Home.jsx
+++ Job-blog/src/Pages/Home.jsx
@@ -1,6 +1,11 @@
 import React, {useState} from "react"
 import Banner from "../components/Banner"
 const Home = () => {
+  const[selectedCategory,setSelectedCategory] = useState(null);
+  const [jobs,setJobs] = useState([]);
+  
+  useEffect
+
   const [query, setQuery] = useState("");
   const handleInputChange = (event) => {
     setQuery(event.target.value);
