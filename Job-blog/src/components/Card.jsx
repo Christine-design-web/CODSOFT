feda2eab83f9a62390584e139ae5adbffc27662a
Christine-design-web <wakuthiichristine5@gmail.com>
2024-02-25 07:57:45 +0300

    text size

--- Job-blog/src/components/Card.jsx
+++ Job-blog/src/components/Card.jsx
@@ -8,7 +8,7 @@ const Card = ({data}) => {
   return (
     <section className= "card">
       <Link to={"/"} className="flex gap-4 flex-col sm:flex-row items-start">
-        <img  className=" w-17 h-12" src={companyLogo} alt="" />
+        <img className=" w-20 h-13" src={companyLogo} alt="" />
         <div>
           <h4 className="text-primary mb-1">{companyName}</h4>
           <h3 className="text-lg font-semibold mb-2">{jobTitle}</h3>
