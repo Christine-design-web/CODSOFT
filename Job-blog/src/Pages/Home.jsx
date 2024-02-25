b4c81aeb3b50f2b91b03e9088b4a5102710ee4a0
Christine-design-web <wakuthiichristine5@gmail.com>
2024-02-25 23:53:33 +0300

    pages error

--- Job-blog/src/Pages/Home.jsx
+++ Job-blog/src/Pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
   const [jobs,setJobs] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 4;
+  const itemsPerPage = 12;
   
   useEffect(()=> {
     setIsLoading(true);
