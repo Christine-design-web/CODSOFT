7423799f0727275be324ab257c4519b223763195
Christine-design-web <wakuthiichristine5@gmail.com>
2024-02-27 21:13:44 +0300

    padding updates

--- Job-blog/src/sidebar/Button.jsx
+++ Job-blog/src/sidebar/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ onClickHandler, value, title }) => {
   return (
-    <button onClick={onClickHandler} value={value} className={"font-bold py-1 px-4 border text-base  hover:bg-blue hover:text-white text-primary "}>
+    <button onClick={onClickHandler} value={value} className={"font-bold py-1 px-4 border text-sm sm:text-sm md:text-base hover:bg-blue hover:text-white text-primary "}>
       {title}
     </button>
   )
