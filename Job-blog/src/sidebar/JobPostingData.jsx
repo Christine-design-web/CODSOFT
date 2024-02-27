cbf81ca12439bb2910560543178c44684b805dbd
Christine-design-web <wakuthiichristine5@gmail.com>
2024-02-27 23:28:00 +0300

    commenting out

--- Job-blog/src/sidebar/JobPostingData.jsx
+++ Job-blog/src/sidebar/JobPostingData.jsx
@@ -3,7 +3,7 @@ import InputField from '../components/InputField'
 
 const JobPostingData = (handleChange) => {
     const now= new Date();
-  console.log(now)
+//   console.log(now)
     const twentyFourHoursAgo = new Date(now - 24 * 60 * 60 * 1000);
     const SevenDaysAgo = new Date(now - 7 * 24 * 60 * 60 * 1000);
     const ThirtyDaysAgo = new Date(now - 30 * 24 * 60 * 601000);
