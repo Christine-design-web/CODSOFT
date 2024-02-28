299703328a54d67596ba4b1fe692284093d71eb8
Christine-design-web <wakuthiichristine5@gmail.com>
2024-02-28 23:08:30 +0300

    mission on the salary button is done

--- Job-blog/src/sidebar/Salary.jsx
+++ Job-blog/src/sidebar/Salary.jsx
@@ -6,7 +6,7 @@ const Salary = ({handleChange, handleClick}) => {
   return (
     <div>
         <h4 className="text-lg font-medium mb-2">Salary</h4>
-        <div className="flex flex-wrap">
+        <div className="flex flex-row">
             <Button onClickHandler={handleClick} value="" title="Hourly"/>
             <Button onClickHandler={handleClick} value="Monthly" title="Monthly" className="pr-4"/>
             <Button onClickHandler={handleClick} value="Yearly" title="Yearly" className="pr-4"/>
